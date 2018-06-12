1cd9bebe72cf7cc4c3725b3e04ba0a0caafcf38e
Ben Asselmeier <benasselmeier@gmail.com>
2018-06-12 17:35:13 -0500

    working on and up to number 2. transferring to desktop.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,10 +1,12 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,10 +23,13 @@ public class JobController {
 
     // The detail display for a given Job at URLs like /job?id=17
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model, int id) {
+    public String index(Model model, @PathVariable int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        // CALL findById from JobData class.
 
+        Job thisJob = jobData.findById(id);
+        model.addAttribute("job", thisJob);
         return "job-detail";
     }
 
